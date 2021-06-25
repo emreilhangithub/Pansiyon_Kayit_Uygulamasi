07b5490a56719aedc91e04c8a8996accb46472fb
Mustafa Emre Ilhan <mustafaemreilhan1903@gmail.com>
2021-06-25 17:58:05 +0300

    Kullanıcı girişi sql ile baglandı ve linq kullanarak kontrol saglandı

--- PansiyonKayitUygulamasi/FrmGirisPaneli.cs
+++ PansiyonKayitUygulamasi/FrmGirisPaneli.cs
@@ -7,6 +7,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.Data.SqlClient;
 
 namespace PansiyonKayitUygulamasi
 {
@@ -17,11 +18,38 @@ namespace PansiyonKayitUygulamasi
             InitializeComponent();
         }
 
+        sqlbaglantisi bgl = new sqlbaglantisi();
+        Kullanici kullanici = new Kullanici();
+
         private void btnGirisYap_Click(object sender, EventArgs e)
         {
-            FrmAnaForm fr = new FrmAnaForm();
-            fr.Show();
-            this.Hide();
+            var result = new List<Kullanici>(); //liste oluşturduk         
+
+            SqlCommand cmd = new SqlCommand("select Kullanici_Adi,Kullanici_Sifre from Tbl_Kullanicilar", bgl.baglanti());
+     
+            SqlDataAdapter da = new SqlDataAdapter(cmd);            
+            DataTable dt = new DataTable();
+            da.Fill(dt);
+
+            result = dt.AsEnumerable().Select(s => new Kullanici
+            {
+                Kullanici_Adi1 = s.Field<string>("Kullanici_Adi"),
+                Kullanici_Sifre1 = s.Field<string>("Kullanici_Sifre")
+            }).ToList();
+
+            var user = result.FirstOrDefault(x => x.Kullanici_Adi1 == txtKullaniciAdi.Text && x.Kullanici_Sifre1 == txtKullaniciSifre.Text);
+            if (user != null)
+            {
+                MessageBox.Show("Giriş başarılı Ana Sayfaya Hoş Geldiniz");
+                FrmAnaForm fr = new FrmAnaForm();
+                fr.Show();
+                this.Hide();
+            }
+            else
+            {
+                MessageBox.Show("Geçersiz Giriş, lütfen kullanıcı adı ve şifreyi kontrol edin");
+            }        
+
         }
     }
 }
